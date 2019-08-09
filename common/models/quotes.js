7b7223f8182bbc6dad7876d1a6997e6df174957f
eryoher <eryoher@gmail.com>
2019-08-08 22:18:50 -0500

    envio de correos

--- common/models/quotes.js
+++ common/models/quotes.js
@@ -2,5 +2,68 @@
 
 const RESTUtils = require('../utils/RESTUtils');
 const ERROR_GENERIC = 'Error conexion con el servidor';
+const Axios = require('axios');
+const sendEmail = require('../utils/sendEmail');
 
-module.exports = function (Quotes) { };
+module.exports = function (Quotes) {
+
+    /**
+   * To search promotions to admin form
+   * @param {object} params data for search
+   * @param {Function(Error, object)} callback
+   */
+
+    Quotes.remoteMethod('createQuote', {
+        accepts: [
+            { arg: 'params', type: 'object', 'description': 'all object data', 'http': { 'source': 'body' } },
+
+        ],
+        returns: {
+            type: 'object',
+            root: true,
+            description: 'response data of service'
+        },
+        description: 'Post current quotation',
+        http: {
+            verb: 'post'
+        },
+    });
+
+    Quotes.createQuote = async function (params) {
+        try {
+            const savedQuote = await Quotes.create(params);
+            const response = await sendContactCRM(params);
+            if (savedQuote) {
+                const product = await Quotes.app.models.Products.findById(savedQuote.productId);                
+                sendEmail.sendquoteBono({ ...params, id: savedQuote.id, product });
+                return savedQuote;
+            }
+        } catch (error) {
+            console.error(error)
+        }
+
+        return null;
+    }
+
+
+    const sendContactCRM = async (params) => {
+        const instance = Axios.create();
+        const customParams = {
+            txtDoc: params.cedula,
+            txtName: params.name,
+            txtCel: params.phone,
+            txtEmail: params.email,
+            txtProductoId: params.productId,
+            accion: 'ins'
+        }
+
+        try {
+            const response = await instance.post('http://admin.cyberdays.com.co/controller/contactosController.php', customParams);
+            return response;
+        } catch (error) {
+            console.error(error)
+        }
+        return response;
+    }
+
+};
--- common/utils/sendEmail.js
+++ common/utils/sendEmail.js
@@ -1,101 +1,53 @@
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable no-undef */
+/* eslint-disable object-curly-spacing */
+/* eslint-disable indent */
+/* eslint-disable space-before-function-paren */
+/* eslint-disable comma-dangle */
 'use strict';
 
 const fs = require('fs');
 const nodemailer = require('nodemailer');
 const handlebars = require('handlebars');
 const config = require('../../server/config.json');
+const CodeGenerator = require('./CodeGenerator');
 const configSmtp = config.configSMTP;
 
-const sendBuyPromotionEmail = ( params ) => {        
-    const transport = nodemailer.createTransport(configSmtp);    
-    
-    readHTMLFile(__dirname + '/../../common/templates/template_buy_promotion_email.html', function(err, html) {            
-        let template = handlebars.compile(html);            
+const sendquoteBono = (params) => {
+    const transport = nodemailer.createTransport(configSmtp);
+    readHTMLFile(__dirname + '/../../common/templates/template_email_sanAutos.html', function (err, html) {
+        let template = handlebars.compile(html);
 
         const replacements = {
-            fullName: params.last_name,
-            code: params.code,
-            discount:params.promotion.discount,
-            promotion:params.promotion.name
+            name: params.name,
+            concecutivo: CodeGenerator.zfill(params.id, 4),
+            product: params.product.name,            
         };
 
-        const htmlToSend = template(replacements);          
-         
+        const htmlToSend = template(replacements);
+
         transport.sendMail({
             from: 'servicioalcliente@fundacionportalmagico.org',
             to: params.email,
-            subject: `Detalles de tu donacion ${params.first_name}`,            
+            subject: 'Detalles del bono',
             html: htmlToSend
         }, function (er) {
-            if (er) console.error(er)
+            if (er) console.error(er);
         });
-    }); 
-    
-}
+    });
+};
 
-
-const sendConfirmationEmail = ( params ) => {    
-    const transport = nodemailer.createTransport(configSmtp);        
-    readHTMLFile(__dirname + '/../../common/templates/template_confirmation_email.html', function(err, html) {            
-        let template = handlebars.compile(html);            
-
-        const replacements = {
-            fullName: params.fullName,
-            urlConfirmation: params.urlConfirmation
-        };
-
-        const htmlToSend = template(replacements);          
-         
-        transport.sendMail({
-            from: 'servicioalcliente@fundacionportalmagico.org',
-            to: params.email,
-            subject: 'Confirmación subscripción.',            
-            html: htmlToSend
-        }, function (er) {
-            if (er) console.error(er)
-        });
-    });      
-    
-}
-
-const sendRecoverEmail = ( params ) => {    
-    const transport = nodemailer.createTransport(configSmtp);        
-    readHTMLFile(__dirname + '/../../common/templates/template_recover_password.html', function(err, html) {            
-        let template = handlebars.compile(html);            
-
-        const replacements = {            
-            urlConfirmation: params.urlRecover,
-            username:params.username
-        };
-
-        const htmlToSend = template(replacements);          
-         
-        transport.sendMail({
-            from: 'servicioalcliente@fundacionportalmagico.org',
-            to: params.email,
-            subject: 'Recuperacion de contraseña',            
-            html: htmlToSend
-        }, function (er) {
-            if (er) console.error(er)
-        });
-    });      
-    
-}
-
-const readHTMLFile = function(path, callback) {
-    fs.readFile(path, {encoding: 'utf-8'}, function (error, html) {
+const readHTMLFile = function (path, callback) {
+    fs.readFile(path, { encoding: 'utf-8' }, function (error, html) {
         if (error) {
-            console.log(error);                
+            console.log(error);
             throw error;
-        }
-        else {
+        } else {
             callback(null, html);
         }
     });
 };
 
 module.exports = {
-    sendConfirmationEmail,  
-    sendBuyPromotionEmail, 
-    sendRecoverEmail 
+    sendquoteBono   
 };
